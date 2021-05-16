0d9aec5e5ce83b3d841fddad3552ed37aaa9b326
leechongyan <leechongyan33@gmail.com>
2021-05-16 18:14:23 +0800

    email template

--- authentication_service/mail_service/mail.go
+++ authentication_service/mail_service/mail.go
@@ -1,45 +1,52 @@
 package mail_service 
 
 import (
-	"crypto/tls"
 	"fmt"
+	"bytes"
+	"net/smtp"
 
 	"github.com/leechongyan/Studtor_backend/authentication_service/models"
 	"github.com/spf13/viper"
-  
-	gomail "gopkg.in/mail.v2"
+	"text/template"  
   )
   
   func SendVerificationCode(user models.User, code string) {
-	m := gomail.NewMessage()
-
 	serverEmail := viper.GetString("serverEmail") 
 	serverEmailPW := viper.GetString("serverEmailPW") 
   
-	// Set E-Mail sender
-	m.SetHeader("From", serverEmail)
-  
-	// Set E-Mail receivers
-	m.SetHeader("To", *user.Email)
-  
-	// Set E-Mail subject
-	m.SetHeader("Subject", "Verify your email")
-  
-	// Set E-Mail body. You can set plain text or html with text/html
-	m.SetBody("text/plain", fmt.Sprintf("This is your verification code: %s", code))
-  
-	// Settings for SMTP server
-	d := gomail.NewDialer("smtp.gmail.com", 587, serverEmail, serverEmailPW)
-  
-	// This is only needed when SSL/TLS certificate is not valid on server.
-	// In production this should be set to false.
-	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
-  
-	// Now send E-Mail
-	if err := d.DialAndSend(m); err != nil {
-	  fmt.Println(err)
-	  panic(err)
+	// Sender data.
+	from := serverEmail
+	password := serverEmailPW
+
+	// Receiver email address.
+	to := []string{
+		*user.Email,
 	}
-  
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	t, _ := template.ParseFiles("../mail_service/template.html")
+	var body bytes.Buffer
+  	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body.Write([]byte(fmt.Sprintf("Subject: Verification Code for Studtor \n%s\n\n", mimeHeaders)))
+
+	t.Execute(&body, struct {
+		Name    string
+		Message string
+	  }{
+		Name:    *user.First_name + " " + *user.Last_name,
+		Message: code,
+	  })
+
+	  // Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}  
+
 	return
   }
